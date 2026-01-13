4fb76e1b124b2c62f5ac0e200e054df44ba9b7a8
atizazshah17-cmd <atizazshah17@gmail.com>
2026-01-13 08:59:53 -0800

    first commit

---
+++ tut3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+// This program was coded by Atizaz Shah
+/*This is
+a multi
+line
+comment*/
+int main() {
+  int sum = 6;
+    std::cout<< "Hello world" << sum;
+    return 0;
+}
