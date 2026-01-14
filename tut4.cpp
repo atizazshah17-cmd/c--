6d17305c925b5fc26e51e6df12d4fff03485aca8
atizazshah17-cmd <atizazshah17@gmail.com>
2026-01-13 23:57:15 -0800

    Add sum function and new tutorial for user input and sum calculation

--- tut4.cpp
+++ tut4.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
 
 using namespace std;
+int glo = 6;
+void sum(){
+    int a;
+}
 
 int main(){
-    int a =4;
-    int b = 5 ;
-    cout<<"This is tytorial 4. Here the value of a is "<<a<<".The value  of b is "<< b;
+    int glo = 9;
+    glo = 78;
+   // int a =4;
+   // int b = 5 ;
+    float pi = 3.14;
+    char c = 'd';
+  //  cout<<"This is tytorial 4.\nHere the value of a is "<<a<<".\nThe value  of b is "<< b;
+   // cout<<"\nThe value of pi: "<<pi;
+   // cout<<"\nThe value of c: "<<c;
     return 0;
 }
