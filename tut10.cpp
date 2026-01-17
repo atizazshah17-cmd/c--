125bf299a001875f2d36d16f50c6889f74066e87
atizazshah17-cmd <atizazshah17@gmail.com>
2026-01-17 00:08:04 -0800

    for white & do while loop in C++

---
+++ tut10.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    /* For loop in c++*/
+    // int i =1;
+    // cout<<i;
+    // i++;
+
+    // Syntax of for loop
+    // for(initialization; condition; updation)
+    //{
+        // loop body(C++ code)
+   // }
+ //for (int i = 1; i <= 40 ; i++)
+ //{
+   /* Code */
+//cout<<i<<endl;
+// }
+// example for infinite for loop
+//   for (int i = 1; 34 <= 40; i++)
+// {
+        /* Code */
+//  cout<<i<<endl;
+// }
+ // example for while loop in C++
+// syntax of while loop
+// while(Condition):
+//{
+// C++ statements; 
+// }
+// printing 1 to 40 using while loop
+//int i = 1;
+//while (i <= 40){
+   // cout<<i<<endl;
+   // i++;
+//}
+/* do while loop in C++*/
+// syntax of do while loop
+int i = 1;
+do{
+    cout<<i<<endl;
+    i++;
+}while(i<=40); 
+    return 0;
+}
