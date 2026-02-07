3611ec3b4299b766df7b0d8dc0ad9baee9b60c03
atizazshah17-cmd <atizazshah17@gmail.com>
2026-02-07 01:11:17 -0800

    C++ object memory allocation & using arrays in classes in C++

---
+++ tut23.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class shop
+{
+    int itemID[100];
+    int itemPrice[100];
+    int counter;
+
+public:
+    void initcounter(void) { counter = 0; }
+    void setPrice(void);
+    void displayPrice(void);
+};
+
+void shop::setPrice(void)
+{
+    cout << "Enter ID of your item no " << counter + 1 << endl;
+    cin >> itemID[counter];
+
+    cout << "Enter Price of your item " << endl;
+    cin >> itemPrice[counter];
+
+    counter++;
+} 
+
+void shop::displayPrice(void)
+{
+    for (int i = 0; i < counter; i++)
+    {
+        cout << "The price of item with ID "
+             << itemID[i] << " is "
+             << itemPrice[i] << endl;
+    }
+}
+
+int main()
+{
+    shop shaheen;
+    shaheen.initcounter();
+
+    shaheen.setPrice();
+    shaheen.setPrice();
+    shaheen.setPrice();
+
+    shaheen.displayPrice();
+    return 0;
+}
