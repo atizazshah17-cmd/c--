3c0ff8ab268be697c4312e7722b3db0945c6e0f2
atizazshah17-cmd <atizazshah17@gmail.com>
2026-02-12 01:33:01 -0800

    friend classes & member friend functions in C++

---
+++ tut27.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+class complex;
+
+class calculator
+{
+public:
+    int add(int a, int b)
+    {
+        return (a + b);
+    }
+
+    int sumRealComplex(complex, complex);
+    int sumCompComplex(complex, complex);
+};
+
+class complex
+{
+    int a, b;
+
+    // Individual declaring the functions as friends
+   // friend int calculator::sumRealComplex(complex o1, complex o2);
+    // friend int calculator::sumCompComplex(complex o1, complex o2);
+    // Aliter : Declaring the entire calculator class as friend
+    friend class calculator;
+public:
+    void setNumber(int n1, int n2)
+    {
+        a = n1;
+        b = n2;
+    }
+
+    void printNumber()
+    {
+        cout << "Your Number is " << a << " + " << b << "i" << endl;
+    }
+};
+
+int calculator::sumRealComplex(complex o1, complex o2)
+{
+    return (o1.a + o2.a);
+}
+int calculator::sumCompComplex(complex o1, complex o2)
+{
+    return (o1.b + o2.b);
+}
+int main()
+{
+    complex o1, o2;  
+
+    o1.setNumber(1, 4);
+    o2.setNumber(5, 8);   
+
+    calculator calc;
+    int result = calc.sumRealComplex(o1, o2);
+    cout << "The sum of real part of o1 and o2 is " << result << endl;
+
+    int result2 = calc.sumCompComplex(o1, o2);
+    cout << "The sum of comp part of o1 and o2 is " << result2 << endl;
+
+    return 0;
+}
