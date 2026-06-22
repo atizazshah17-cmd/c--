bd63bec9228177da59dabd718716f5f65ba3618e
atizazshah17-cmd <atizazshah17@gmail.com>
2026-06-22 00:33:27 -0700

    Function template and Function templat wit parameter in C++

---
+++ tut67.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+/*
+float funcAverage(int a, int b)
+{
+    float avg = (a + b) / 2.0;
+    return avg;
+}
+
+float funcAverage2(int a, float b)
+{
+    float avg = (a + b) / 2.0;
+    return avg;
+}
+*/
+
+template <class T>
+void swapp(T &a , T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <class T1 , class T2>
+float funcAverage(T1 a, T2 b)
+{
+    return (a + b) / 2.0;
+}
+
+int main()
+{
+    float a;
+    a = funcAverage(5, 2.4);
+
+    printf("The average of these number is %.3f\n", a);
+    int x = 5 ,  y = 7;
+    swapp(x, y);
+    cout <<x << endl <<y;
+
+    return 0;
+}
